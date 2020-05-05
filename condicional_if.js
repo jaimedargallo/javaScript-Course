db1760ef92597a81f376dd16ae1714c848550781
Jaime Dargallo <jaimedargallo@gmail.com>
2020-05-05 19:49:26 +0200

    Add files via upload

---
+++ condicional_if.js
@@ -0,0 +1,44 @@
+/*
+if(condicion){
+
+} else if() {
+
+} else if() {
+
+} else {
+
+}
+
+*/
+
+var numero = 10;
+
+/*
+
+if (numero > 2) {
+
+	console.log('Numero SI es mayor que 2');
+
+} else if ( numero == 2 ){
+
+	console.log('Numero es igual que 2');
+
+} else {
+
+console.log('Numero NO es mayor o igual que 2');
+
+}
+
+*/
+
+if (numero > 2 || numero == 10) {
+
+	console.log('Se cumple la condición')
+}
+
+
+
+
+
+
+
---
+++ metodosPropiedadesArreglos.js
@@ -0,0 +1,37 @@
+// METODOS Y PROPIEDADES PARA ARREGLOS
+
+var familia = ['Alejandro', 'Maria', 'Pedro'];
+var familiaDos = ['Tomas', 'Saul'];
+
+// length 
+
+var cantidadElementosFamilia = familia.length
+
+// concat 
+
+var nuevaFamilia = familia.concat(familiaDos);
+
+// join
+
+var texto = familia.join(' ');
+
+// pop
+
+var familiaIncompleta = familia.pop();
+
+// push
+
+var familiaMasGrande = familia.push('Carlos');
+
+// shift
+
+var familiaElimina = familia.shift();
+
+// unshift
+
+var familiaUltimo = familia.unshift();
+
+// reverse
+
+var reverso = familia.reverse();
+
